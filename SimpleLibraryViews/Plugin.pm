66ee39ad2cdde136602b18f2ad2ae9868cc5d449
Andy Hawkins <andy@gently.org.uk>
2016-05-16 20:48:12 +0100

    Add GPL licence to each source file

--- SimpleLibraryViews/Plugin.pm
+++ SimpleLibraryViews/Plugin.pm
@@ -1,9 +1,22 @@
-package Plugins::SimpleLibraryViews::Plugin;
+# Simple Library Views plugin for Logitech Media Server
+# Copyright (C) 2016 Andy Hawkins
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Andy Hawkins - andy@gently.org.uk
 
-# Logitech Media Server Copyright 2001-2014 Logitech.
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License,
-# version 2.
+package Plugins::SimpleLibraryViews::Plugin;
 
 use strict;
 
