ee37dc68aeab3cc62045547da138580317943a3f
Andy Hawkins <andy@gently.org.uk>
2022-05-31 16:30:07 +0100

    Update build script

--- build.pl
+++ build.pl
@@ -2,63 +2,93 @@
 
 use strict;
 
-use Data::Dump;
 use Digest::SHA;
 use File::Spec::Functions;
 use File::Slurp;
 use LWP::Simple;
-use XML::Simple;
+use XML::LibXML;
 
-use constant REPO => 'repo.xml';
-use constant REPO_URL => 'http://adhawkins.github.io/SimpleLibraryViews/' . REPO;
+use constant DEFAULT_REPO => 'repo';
 
 my $dir = shift @ARGV;
+my $repoName = shift @ARGV;
+
+if (! length $repoName) {
+ $repoName = DEFAULT_REPO;
+}
+
+my $REPO_URL = "http://adhawkins.github.io/SimpleLibraryViews/$repoName.xml";
+
+print "Dir: '$dir', repo: '" . $REPO_URL . "'\n";
 
 if (-d $dir) {
-	my $file = "$dir.zip";
 	my $manifest = catfile($dir, 'install.xml');
-	my $version = '';
 
 	if (-f $manifest) {
-		my $xml = XMLin($manifest);
-		if ($xml && ($version = $xml->{version})) {
-			$file = "$dir-$version.zip";
+		my $xml = XML::LibXML->load_xml(location => $manifest);
+		if ($xml) {
+			my $version = $xml->find('/extension/version')->to_literal();
+			my $minTarget = $xml->find('/extension/targetApplication/minVersion')->to_literal();
+			my $maxTarget = $xml->find('/extension/targetApplication/maxVersion')->to_literal();
+			my $homepageURL = $xml->find('/extension/homepageURL')->to_literal();
+			my $creator = $xml->find('/extension/creator')->to_literal();
+			print("Version: '$version', min: '$minTarget', max: '$maxTarget'\n");
+			my $file = "$dir-$version.zip";
+
+			print "\nCompressing '$dir' to '$file'...\n\n";
+			my @exclude = qw(
+				--exclude=*.git*
+				--exclude=*.DS_Store
+				--exclude=*.svn*
+				--exclude=*/.*
+			);
+
+			if (-e "$dir/exclude.lst") {
+				push @exclude, "--exclude=$dir/exclude.lst", "--exclude=\@$dir/exclude.lst";
+			}
+
+			system('zip', '-q', '-r9', @exclude, $file, $dir);
+
+			if ($?) {
+				warn "Something went wrong: $?\n";
+			}
+			else {
+				print "\nCalculating SHA checksum...\n";
+				my $sha = Digest::SHA->new(1)->addfile($file)->hexdigest;
+				print "$sha\t$file\n";
+
+				my $repo = XML::LibXML->load_xml(location => $REPO_URL);
+
+				my $pluginNode = $repo->find('/extensions/plugins/plugin')->get_node(1);
+				$pluginNode->setAttribute('name', $dir);
+				$pluginNode->setAttribute('version', $version);
+				$pluginNode->setAttribute('minTarget', $minTarget);
+				$pluginNode->setAttribute('maxTarget', $maxTarget);
+
+				my $pluginURLNode = $repo->find('/extensions/plugins/plugin/url')->get_node(1);
+				$pluginURLNode->removeChildNodes();
+				$pluginURLNode->appendText("http://adhawkins.github.io/SimpleLibraryViews/$file");
+
+				my $pluginLinkNode = $repo->find('/extensions/plugins/plugin/link')->get_node(1);
+				$pluginLinkNode->removeChildNodes();
+				$pluginLinkNode->appendText($homepageURL);
+
+				my $pluginCreatorNode = $repo->find('/extensions/plugins/plugin/creator')->get_node(1);
+				$pluginCreatorNode->removeChildNodes();
+				$pluginCreatorNode->appendText($creator);
+
+				my $pluginSHANode = $repo->find('/extensions/plugins/plugin/sha')->get_node(1);
+				$pluginSHANode->removeChildNodes();
+				$pluginSHANode->appendText($sha);
+
+				$repo->toFile("$repoName.xml", 2);
+				print $repo->toString(2);
+			}
+		} else {
+			warn "Error loading manifest: '$manifest'\n";
 		}
-	}
-
-	print "\nCompressing '$dir' to '$file'...\n\n";
-	my @exclude = qw(
-		--exclude=*.git*
-		--exclude=*.DS_Store
-		--exclude=*.svn*
-		--exclude=*/.*
-	);
-
-	if (-e "$dir/exclude.lst") {
-		push @exclude, "--exclude=$dir/exclude.lst", "--exclude=\@$dir/exclude.lst";
-	}
-
-	system('zip', '-r9', @exclude, $file, $dir);
-
-	if ($?) {
-		warn "Something went wrong: $?\n";
-	}
-	else {
-		print "\nCalculating SHA checksum...\n";
-		my $sha = Digest::SHA->new(1)->addfile($file)->hexdigest;
-		print "$sha\t$file\n";
-
-		if ( my $repo = get(REPO_URL) ) {
-			print "\nWriting new repository file...";
-			$repo =~ s/(plugin name="$dir".*?sha>)[\da-f]+/$1$sha/si;
-			$repo =~ s/(plugin name="$dir".*?version=")[\d\.]+/$1$version/si;
-			$repo =~ s/($dir-?[\d\.]*\.zip)/$file/sig;
-			write_file(REPO, {binmode => ':utf8'}, $repo);
-
-			my ($newRepo) = $repo =~ /(\s*<plugin name="$dir.*?\/plugin>)/si;
-			print $newRepo;
-		}
-		print "\nDone!\n";
+	} else {
+		warn "No manifest found in '$dir'\n";
 	}
 }
 else {
